a5808e4e19d614c0a88a19ec19f1473f984b1596
guilhermevesentini <guilherme1992@hotmail.com>
2024-03-12 14:13:34 -0300

    init with create-stencil v3.5.4

---
+++ stencil.config.ts
@@ -0,0 +1,24 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'f360-biblioteca',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+  testing: {
+    browserHeadless: "new",
+  },
+};
